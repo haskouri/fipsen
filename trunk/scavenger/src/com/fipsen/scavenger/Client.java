6a67b86d5062af27d1e67035f6baee5c5ed07f67
fares071 <fares071@d8a22174-ed13-11dd-a738-5fe2e3cfbcc5>
2009-01-28 21:24:31 +0000

    correction on scavenger.

---
+++ trunk/scavenger/src/com/fipsen/scavenger/Client.java
@@ -0,0 +1,28 @@
+package com.fipsen.scavenger;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.fipsen.scavenger.services.HtmlDownloader;
+
+public class Client 
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "fipsen-core.xml" });
+
+		HtmlDownloader htmlDownloader = (HtmlDownloader) context.getBean("downloader");
+		try 
+		{
+			htmlDownloader.download();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
