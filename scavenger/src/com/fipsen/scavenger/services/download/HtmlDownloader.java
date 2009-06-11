cd3fedffc2a3a3f6d4c9b0c1b9ab4fe193cfc4b1
fares071 <fares071@d8a22174-ed13-11dd-a738-5fe2e3cfbcc5>
2009-06-11 15:39:26 +0000

    committing all.

--- scavenger/src/com/fipsen/scavenger/services/download/HtmlDownloader.java
+++ scavenger/src/com/fipsen/scavenger/services/download/HtmlDownloader.java
@@ -1,8 +1,50 @@
 //package com.fipsen.scavenger.services;
 package com.fipsen.scavenger.services.download;
 
-public interface HtmlDownloader 
+import com.fipsen.core.util.Writer;
+
+public abstract class HtmlDownloader 
 {
-	public void download() throws Exception;
+	private Writer writer;
+	private String url;
+	private String downloadDirectory;
+	private String fileExtension;
+
+	public Writer getWriter() 
+	{
+		return writer;
+	}
+
+	public void setWriter(Writer writer) {
+		this.writer = writer;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDownloadDirectory() {
+		return downloadDirectory;
+	}
+
+	public void setDownloadDirectory(String downloadDirectory) {
+		this.downloadDirectory = downloadDirectory;
+	}
+	
+
+	public String getFileExtension() {
+		return fileExtension;
+	}
+
+	public void setFileExtension(String fileExtension) {
+		this.fileExtension = fileExtension;
+	}
+	
+	
+	public abstract String download() throws Exception;
 
 }
